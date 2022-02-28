38a91e37c6556512c4d083535a50b97f51ced3aa
Zachary <zsakowitz@zsnout.com>
2022-02-28 11:36:07 -0800

    Add `FullscreenDisplay` component

---
+++ src/components/FullscreenDisplay.vue
@@ -0,0 +1,17 @@
+<template>
+  <main>
+    <slot />
+  </main>
+</template>
+
+<style scoped>
+  main {
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    padding: 0;
+  }
+</style>
