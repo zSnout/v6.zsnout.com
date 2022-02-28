40a850f9bc543c5329926bcfad77e47834f153f9
Zachary <zsakowitz@zsnout.com>
2022-02-28 08:05:28 -0800

    Add home link to 404 page

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,3 +1,8 @@
+<script setup>
+  import { RouterLink } from "vue-router";
+</script>
+
 <template>
   <h1>Well, you've found a broken link...</h1>
+  <RouterLink to="/">Home</RouterLink>
 </template>
