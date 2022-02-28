afbb826ca82d72cf9a209dbea5f10fe6fe9fa0c8
Zachary <zsakowitz@zsnout.com>
2022-02-28 11:36:22 -0800

    Add `DocumentDisplay` component

---
+++ src/components/DocumentDisplay.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="safe-area">
+    <main>
+      <slot />
+    </main>
+  </div>
+</template>
+
+<style scoped>
+  .safe-area {
+    --padding: 12px;
+    padding: 12px;
+    padding-left: max(var(--padding), env(safe-area-inset-left));
+    padding-right: max(var(--padding), env(safe-area-inset-right));
+    padding-top: max(var(--padding), env(safe-area-inset-top));
+    padding-bottom: max(var(--padding), env(safe-area-inset-bottom));
+    display: flex;
+    min-height: calc(100% - 2 * var(--padding));
+    min-height: calc(
+      100% - max(var(--padding), env(safe-area-inset-top)) -
+        max(var(--padding), env(safe-area-inset-bottom))
+    );
+  }
+
+  main {
+    max-width: 980px;
+    padding: 0;
+    margin: 0 auto;
+    flex: 1;
+  }
+</style>
