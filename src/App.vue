6c0bf20a1c5ef74281752eedd75c50da2eb7e62e
Zachary <zsakowitz@zsnout.com>
2022-02-28 11:35:55 -0800

    Add default font to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,21 @@
-<script setup lang="ts">
+<script setup>
   import { RouterView } from "vue-router";
 </script>
 
 <template>
   <RouterView />
 </template>
+
+<style>
+  html,
+  body,
+  #app {
+    width: 100%;
+    height: 100%;
+    margin: 0;
+  }
+
+  body {
+    font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+  }
+</style>
