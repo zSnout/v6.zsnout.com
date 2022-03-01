ed6d145b0db0ad4ebc304273a464e008b6b6f885
Zachary <zsakowitz@zsnout.com>
2022-02-28 21:02:42 -0800

    Add `SafeAreaLR` component

---
+++ src/components/SafeAreaLR.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="safe-area">
+    <div class="constrained">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+  .safe-area {
+    --padding: 12px;
+    padding: 0 12px;
+    padding-left: max(var(--padding), env(safe-area-inset-left));
+    padding-right: max(var(--padding), env(safe-area-inset-right));
+
+    @media screen and (min-width: 400px) and (min-height: 400px) {
+      --padding: 24px;
+    }
+
+    @media screen and (min-width: 600px) and (min-height: 600px) {
+      --padding: 36px;
+    }
+  }
+
+  .constrained {
+    max-width: 980px;
+    margin: 0 auto;
+  }
+</style>
