7ede7c1009db3d486b713d622443eb3415c2e221
Zachary <zsakowitz@zsnout.com>
2022-03-01 10:27:17 -0800

    Add MatchMedia

---
+++ src/components/MatchMedia.vue
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import { ref } from "vue";
+
+  export function mediaRef(query: string) {
+    let media = matchMedia(query);
+    let mediaRef = ref(media.matches);
+    media.onchange = () => (mediaRef.value = media.matches);
+    return mediaRef;
+  }
+</script>
+
+<script setup lang="ts">
+  defineProps<{ query: string }>();
+</script>
+
+<template>
+  <slot :class="{ hide: !mediaRef(query).value }" />
+</template>
+
+<style>
+  .hide {
+    display: none;
+  }
+</style>
