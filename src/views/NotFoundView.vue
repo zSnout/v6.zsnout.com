47901039ef60bb1d12837a45fb1ee7db2e70a547
Zachary <zsakowitz@zsnout.com>
2022-02-28 21:02:31 -0800

    Add `DocumentDisplay` to 404

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,8 +1,11 @@
 <script setup>
   import { RouterLink } from "vue-router";
+  import DocumentDisplay from "../components/DocumentDisplay.vue";
 </script>
 
 <template>
-  <h1>Well, you've found a broken link...</h1>
-  <RouterLink to="/">Home</RouterLink>
+  <DocumentDisplay>
+    <h1>Well, you've found a broken link...</h1>
+    <RouterLink to="/">Home</RouterLink>
+  </DocumentDisplay>
 </template>
