ee9d3aa570858f3a65820c9e9a8279ee3a417248
Zachary <zsakowitz@zsnout.com>
2022-03-02 06:49:38 -0800

    Add some stuff from original zSnout

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,14 +3,33 @@
 </script>
 
 <template>
+  <link
+    rel="stylesheet"
+    href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&display=swap"
+  />
+
   <DocumentDisplay>
     <h1>Welcome to zSnout!</h1>
+    <p>
+      zSnout is collection of different projects created for fun by Zachary
+      Sakowitz during COVID-19. Our source code is publicly available on our
+      <a href="https://gitbub.com/zSnout/zsnout.com">GitHub page</a>, meaning
+      that anybody can help out by contributing code or reporting an
+      <a href="https://github.com/zSnout/zsnout.com/issues/new/choose"
+        >issue or bug</a
+      >. You can also
+      <a href="https://store.zsnout.com/">buy a fractal shirt</a>.
+    </p>
   </DocumentDisplay>
 </template>
 
-<style>
+<style scoped>
   h1 {
     margin: 0;
     color: var(--accent-color);
+    font-family: "Montserrat", "Helvetica Neue", Helvetica, Arial, sans-serif;
+    font-weight: 700;
+    padding-bottom: 0.25em;
+    border-bottom: 1px solid var(--accent-color);
   }
 </style>
