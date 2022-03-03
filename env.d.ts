425c2dba5b61c6ad277f41ef9186cbe62995325c
Zachary <zsakowitz@zsnout.com>
2022-03-02 17:36:38 -0800

    Add beforeinstallprompt typings

--- env.d.ts
+++ env.d.ts
@@ -4,7 +4,21 @@
 import { CSSProperties, Ref } from "vue";
 
 declare global {
-  declare type StyleValueRefs = {
+  type StyleValueRefs = {
     [K in keyof CSSProperties]: CSSProperties[K] | Ref<CSSProperties[K]>;
   };
+
+  interface BeforeInstallPromptEvent extends Event {
+    readonly platforms: string[];
+    readonly userChoice: Promise<{
+      readonly outcome: "accepted" | "dismissed";
+      readonly platform: string;
+    }>;
+
+    prompt(): Promise<void>;
+  }
+
+  interface WindowEventMap {
+    beforeinstallprompt: BeforeInstallPromptEvent;
+  }
 }
