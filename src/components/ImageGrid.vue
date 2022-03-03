54105a6040009ba70b44abcf766bc2e167c96693
Zachary <zsakowitz@zsnout.com>
2022-03-02 22:41:08 -0800

    Fix ImageGrid

--- src/components/ImageGrid.vue
+++ src/components/ImageGrid.vue
@@ -1,10 +1,50 @@
+<script lang="ts">
+  let onResizeListeners = new Set<() => void>();
+
+  window.addEventListener(
+    "resize",
+    () => onResizeListeners.forEach((cb) => cb()),
+    { passive: true }
+  );
+</script>
+
+<script setup lang="ts">
+  import { onMounted, onUnmounted, reactive, ref } from "vue";
+
+  let { cols, rows } = defineProps<{ cols: number; rows: number }>();
+  let grid = ref<HTMLDivElement | null>(null);
+  let styles = reactive({
+    gridTemplateColumns: `repeat(${cols}, 1fr)`,
+    gridTemplateRows: `repeat(${rows}, 1fr)`,
+    height: "initial",
+  });
+
+  function onResize() {
+    let width = grid.value?.scrollWidth;
+    if (!width) return;
+    let height = (width / cols) * rows;
+
+    styles.height = `${height}px`;
+  }
+
+  onMounted(() => {
+    onResizeListeners.add(onResize);
+    onResize();
+  });
+
+  onUnmounted(() => {
+    onResizeListeners.delete(onResize);
+  });
+</script>
+
 <template>
-  <div class="image-grid"></div>
+  <div ref="grid" class="image-grid" :style="styles">
+    <slot />
+  </div>
 </template>
 
 <style scoped>
   .image-grid {
     display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(16px, 1fr));
   }
 </style>
