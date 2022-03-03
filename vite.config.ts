04fa602edc377bd774d223dfd0d24e09085f3066
Zachary <zsakowitz@zsnout.com>
2022-03-02 17:08:45 -0800

    Precache public assets

--- vite.config.ts
+++ vite.config.ts
@@ -3,22 +3,42 @@ import vueJsx from "@vitejs/plugin-vue-jsx";
 import { fileURLToPath, URL } from "url";
 import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
+import glob from "glob";
 
-export default defineConfig({
-  publicDir: fileURLToPath(new URL("./public", import.meta.url)),
-  plugins: [
-    vue(),
-    vueJsx(),
-    VitePWA({
-      includeAssets: ["favicon.ico", "apple-touch-icon.png"],
-      devOptions: { enabled: false },
-      workbox: { cleanupOutdatedCaches: true },
-      manifest: false,
-    }),
-  ],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
-  },
-});
+let publicDir = fileURLToPath(new URL("./public", import.meta.url));
+let revision = Math.random().toString().slice(2);
+export default new Promise(async (resolve) =>
+  resolve(
+    defineConfig({
+      publicDir: publicDir,
+      plugins: [
+        vue(),
+        vueJsx(),
+        VitePWA({
+          devOptions: { enabled: false },
+          manifest: false,
+          workbox: {
+            navigateFallback: "/index.html",
+            cleanupOutdatedCaches: true,
+            sourcemap: false,
+            clientsClaim: true,
+            skipWaiting: true,
+            additionalManifestEntries: await new Promise<string[]>((resolve) =>
+              glob(`${publicDir}/**/*`, (_err, matches) => resolve(matches))
+            ).then((matches) =>
+              matches.map((url) => ({
+                url: url.slice(publicDir.length + 1),
+                revision,
+              }))
+            ),
+          },
+        }),
+      ],
+      resolve: {
+        alias: {
+          "@": fileURLToPath(new URL("./src", import.meta.url)),
+        },
+      },
+    })
+  )
+);
