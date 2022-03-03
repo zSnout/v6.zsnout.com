e400c8d77b603fc7c43eca3b9e4db644ebb8f850
Zachary <zsakowitz@zsnout.com>
2022-03-02 23:57:11 -0800

    Externalize onMounted of FSCanvas

--- src/components/FSCanvas.vue
+++ src/components/FSCanvas.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
-  import { ref } from "vue";
+  import { onMounted, ref } from "vue";
   import FullscreenDisplay from "./FullscreenDisplay.vue";
 
-  defineEmits<{ (event: "ready", canvas: HTMLCanvasElement): void }>();
+  let emit =
+    defineEmits<{ (event: "ready", canvas: HTMLCanvasElement): void }>();
   let canvas = ref<HTMLCanvasElement | null>(null);
 
   function onResize() {
@@ -13,6 +14,13 @@
     }
   }
 
+  onMounted(() => {
+    if (canvas.value) {
+      onResize();
+      emit("ready", canvas.value);
+    }
+  });
+
   window.addEventListener("resize", onResize);
 </script>
 
@@ -22,9 +30,6 @@
       <slot name="nav" />
     </template>
 
-    <canvas
-      ref="canvas"
-      @vnode-mounted="canvas && onResize() && $emit('ready', canvas)"
-    />
+    <canvas ref="canvas" />
   </FullscreenDisplay>
 </template>
