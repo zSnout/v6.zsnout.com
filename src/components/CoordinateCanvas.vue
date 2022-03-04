124e7f8785f7044e0310a5ab0086a0737a7541e2
Zachary <zsakowitz@zsnout.com>
2022-03-03 16:12:49 -0800

    Add movable coordinate system to CoordinateCanvas

--- src/components/CoordinateCanvas.vue
+++ src/components/CoordinateCanvas.vue
@@ -1,6 +1,19 @@
 <script setup lang="ts">
+  import { router } from "@/main";
   import { unref, type Ref } from "vue";
   import WebGL2Canvas, { type WebGl2ProgramInfo } from "./WebGL2Canvas.vue";
+
+  export interface CoordinateCanvasInfo extends WebGl2ProgramInfo {
+    mouseToCoords(
+      mouseXPercent: number,
+      mouseYPercent: number
+    ): [coordX: number, coordY: number];
+
+    getCoords(): CoordinateList;
+    updateCoords(coords: CoordinateList): void;
+    getSaveCode(): string;
+    loadSaveCode(code: string): void;
+  }
 
   export interface CoordinateList {
     xStart: number;
@@ -14,19 +27,20 @@
     offset: [xOffset: number, yOffset: number];
   }
 
-  let emit = defineEmits<{ (event: "ready", info: WebGl2ProgramInfo): void }>();
+  let emit =
+    defineEmits<{ (event: "ready", info: CoordinateCanvasInfo): void }>();
   let { shader } = defineProps<{ shader: string | Ref<string> }>();
 
   let coordConvertShader = `
-  #version 300 es
-  precision highp float;
+    #version 300 es
+    precision highp float;
 
-  uniform vec2 scale;
-  uniform vec2 offset;
-  vec2 convert(vec2 pos) {
-    return pos * scale + offset;
-  }
-  `;
+    uniform vec2 scale;
+    uniform vec2 offset;
+    vec2 convert(vec2 pos) {
+      return pos * scale + offset;
+    }
+    `;
 
   function onReady({ gl, canvas, program, render }: WebGl2ProgramInfo) {
     let [xStart, xEnd, yStart, yEnd] = [-2, 2, -2, 2];
@@ -59,25 +73,84 @@
       };
     }
 
-    emit("ready", {
-      gl,
-      canvas,
-      program,
-      render() {
-        let { scale, offset } = computeScaleAndOffset();
-        gl.uniform2f(scaleLoc, ...scale);
-        gl.uniform2f(offsetLoc, ...offset);
-        render();
-      },
-    });
-
-    // I know that the resize event isn't dispatched on canvases, but FSCanvas does it for us.
-    canvas.addEventListener("resize", () => {
+    function renderCanvas() {
       let { scale, offset } = computeScaleAndOffset();
       gl.uniform2f(scaleLoc, ...scale);
       gl.uniform2f(offsetLoc, ...offset);
       render();
+    }
+
+    function mouseToCoords(
+      mouseXPercent: number,
+      mouseYPercent: number
+    ): [coordX: number, coordY: number] {
+      let { scale, offset } = computeScaleAndOffset();
+      let x = (mouseXPercent * 2 - 1) * scale[0] + offset[0];
+      let y = ((1 - mouseYPercent) * 2 - 1) * scale[1] + offset[1];
+      return [x, y];
+    }
+
+    function updateCoords(coords: CoordinateList) {
+      ({ xStart, xEnd, yStart, yEnd } = coords);
+      renderCanvas();
+    }
+
+    function getSaveCode() {
+      return `${xStart},${xEnd},${yStart},${yEnd}`;
+    }
+
+    function loadSaveCode(code: string) {
+      let [xs, xe, ys, ye] = code.split(",").map(Number);
+
+      if (!isNaN(xs) && !isNaN(xe) && !isNaN(ys) && !isNaN(ye))
+        updateCoords({ xStart: xs, xEnd: xe, yStart: ys, yEnd: ye });
+    }
+
+    emit("ready", {
+      gl,
+      canvas,
+      program,
+      updateCoords,
+      mouseToCoords,
+      getSaveCode,
+      loadSaveCode,
+      getCoords: computeEndpoints,
+      render: renderCanvas,
     });
+
+    let isMouseDown = false;
+
+    canvas.addEventListener("mousedown", (e) => {
+      if (e.button == 0) {
+        isMouseDown = true;
+        return;
+      }
+    });
+
+    canvas.addEventListener("mousemove", ({ movementX, movementY }) => {
+      if (!isMouseDown) return;
+
+      // Have to rename locals as `computeEndpoints` returns something different than globals
+      let { xStart: xs, xEnd: xe, yStart: ys, yEnd: ye } = computeEndpoints();
+      let xChange =
+        (-((xe - xs) * movementX) / canvas.width) * devicePixelRatio;
+      let yChange =
+        (((ye - ys) * movementY) / canvas.height) * devicePixelRatio;
+
+      updateCoords({
+        xStart: xStart + xChange,
+        xEnd: xEnd + xChange,
+        yStart: yStart + yChange,
+        yEnd: yEnd + yChange,
+      });
+    });
+
+    window.addEventListener("mouseup", (e) => {
+      if (e.button == 0) return (isMouseDown = false);
+    });
+
+    // The FSCanvas component dispatches `resize` after update the canvas's size.
+    canvas.addEventListener("resize", renderCanvas);
   }
 </script>
 
