eeb89b18320d0c0c7507d29b108c4d0f0b8da8ca
Zachary <zsakowitz@zsnout.com>
2022-03-06 08:06:34 -0800

    Add MultDivQuiz view

---
+++ src/views/MultDivQuiz.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+  import type { Question } from "@/components/Quiz.vue";
+  import Quiz from "../components/Quiz.vue";
+
+  function generator(): Question {
+    let a = Math.floor(11 * Math.random()) + 2;
+    let b = Math.floor(11 * Math.random()) + 2;
+    let c = a * b;
+
+    if (Math.random() < 0.5)
+      return {
+        prefix: `${a} × ${b} = `,
+        answers: ["" + c],
+      };
+    else
+      return {
+        prefix: `${c} ÷ ${a} = `,
+        answers: ["" + b],
+      };
+  }
+</script>
+
+<template>
+  <Quiz :generator="generator" />
+</template>
