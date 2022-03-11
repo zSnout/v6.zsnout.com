b9fda63ead0455283def89407fc59355b9193ce5
Zachary <zsakowitz@zsnout.com>
2022-03-11 14:37:12 -0800

    Add flip button

--- src/views/StandardChessboardView.vue
+++ src/views/StandardChessboardView.vue
@@ -1,7 +1,24 @@
-<script setup>
+<script setup lang="ts">
   import LegalMoveChessboard from "@/components/LegalMoveChessboard.vue";
+  import type { ChessInstance } from "chess.js";
+  import type { Api } from "chessground/api";
+  import NavLink from "../components/NavLink.vue";
+
+  let _flip: (() => void) | undefined;
+
+  function onReady(api: Api, game: ChessInstance) {
+    _flip = () => api.toggleOrientation();
+  }
+
+  function flip() {
+    _flip?.();
+  }
 </script>
 
 <template>
-  <LegalMoveChessboard />
+  <LegalMoveChessboard @ready="onReady">
+    <template #nav>
+      <NavLink @click="flip">Flip Board</NavLink>
+    </template>
+  </LegalMoveChessboard>
 </template>
