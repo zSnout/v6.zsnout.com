f2550b9d4320aa62d59880059627786dfa03a93d
Zachary <zsakowitz@zsnout.com>
2022-03-10 17:44:02 -0800

    Remove unused imports

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-  import DocumentDisplay from "@/components/DocumentDisplay.vue";
   import { onMounted, onUnmounted, ref } from "vue";
   import { RouterLink } from "vue-router";
 
