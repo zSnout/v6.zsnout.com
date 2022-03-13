531e04abfda3b3fe5150ead16adf5e877b295644
Zachary <zsakowitz@zsnout.com>
2022-03-13 16:10:26 -0700

    Add better styling to SearchItem

--- src/components/SearchItem.vue
+++ src/components/SearchItem.vue
@@ -6,22 +6,42 @@
 
 <template>
   <a v-if="to.includes('://')" :href="to" class="item" target="_blank">
-    {{ name }}
+    <div class="name">{{ name }}</div>
+    <div class="desc">{{ desc }}</div>
   </a>
 
   <RouterLink v-else :to="to" class="item">
-    {{ name }}
+    <div class="name">{{ name }}</div>
+    <div class="desc">{{ desc }}</div>
   </RouterLink>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+  @use "@/assets/util.scss" as *;
+
   .item {
     display: inline-block;
-    padding: 0.25em 0.5em;
-    background: var(--field-background);
-    border-radius: 4px;
-    color: var(--accent-color);
     flex: auto;
-    text-align: center;
+    padding: 0.5em 0.75em;
+    color: inherit;
+    text-decoration: none;
+    background-color: var(--pure-background);
+    border-radius: 4px;
+    box-shadow: 0 3px 6px 1px var(--shadow-color);
+    transition: box-shadow 0.3s;
+
+    @include focus {
+      box-shadow: 0 0 6px 2px var(--accent-color-light);
+    }
+  }
+
+  .name {
+    margin-bottom: 0.25em;
+    font-weight: bold;
+  }
+
+  .desc {
+    color: var(--text-color-light);
+    font-size: 0.9em;
   }
 </style>
