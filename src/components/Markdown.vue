b0e3abf8d2db1010dc903102c26686137019b0d8
Zachary <zsakowitz@zsnout.com>
2022-03-16 10:20:30 -0700

    Add `Markdown` component

---
+++ src/components/Markdown.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+  import MarkdownIt from "markdown-it";
+  let instance = MarkdownIt();
+
+  defineProps<{ source: string }>();
+</script>
+
+<template>
+  <div class="markdown" v-html="instance.render(source)" />
+</template>
