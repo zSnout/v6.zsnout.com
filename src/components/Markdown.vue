e9d2501147cb7fe82a9266db01acad86d263ccce
Zachary <zsakowitz@zsnout.com>
2022-03-16 17:15:35 -0700

    Add `dedent` function

--- src/components/Markdown.vue
+++ src/components/Markdown.vue
@@ -1,3 +1,13 @@
+<script lang="ts">
+  export function dedent([content]: TemplateStringsArray) {
+    return content
+      .split("\n")
+      .map((line) => line.slice(2))
+      .join("\n")
+      .trim();
+  }
+</script>
+
 <script setup lang="ts">
   import MarkdownIt from "markdown-it";
   let instance = MarkdownIt();
