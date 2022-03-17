4ac98b83ed31f36b70170ceb81611cb2cbd905ff
Zachary <zsakowitz@zsnout.com>
2022-03-16 20:28:13 -0700

    Use MarkdownItAnchor and MarkdownItTOC

--- src/components/Markdown.vue
+++ src/components/Markdown.vue
@@ -12,7 +12,12 @@
 
 <script setup lang="ts">
   import MarkdownIt from "markdown-it";
-  let instance = MarkdownIt();
+  import MarkdownItAnchor from "markdown-it-anchor";
+  import MarkdownItTOC from "markdown-it-toc-done-right";
+
+  let instance = MarkdownIt()
+    .use(MarkdownItAnchor)
+    .use(MarkdownItTOC, { listType: "ul" });
 
   defineProps<{ source: string }>();
 </script>
@@ -24,7 +29,31 @@
 <style scoped lang="scss">
   @import url("https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,700;1,400;1,700&display=swap");
 
-  .markdown {
+  .markdown :deep() {
+    .table-of-contents {
+      background-color: var(--field-background);
+      border: 1px solid var(--border-color);
+      border-radius: 0.25em;
+
+      > ul {
+        margin: 0.5em 0;
+        list-style-type: disc;
+      }
+
+      a {
+        color: inherit;
+      }
+    }
+
+    img {
+      display: block;
+      max-width: 100%;
+      max-height: 20em;
+      margin: 0 auto;
+      border: 0.0625em solid var(--border-color);
+      border-radius: 0.25em;
+    }
+
     h1,
     h2,
     h3,
@@ -42,7 +71,7 @@
     h3 {
       margin: 0.67em 0;
       padding-bottom: 0.125em;
-      border-bottom: 0.0625em solid var(--accent-color);
+      border-bottom: 0.0625em solid var(--border-color);
     }
 
     h1 {
@@ -105,7 +134,7 @@
     hr {
       margin: 1em 0;
       border: 0;
-      border-top: 0.125em solid var(--accent-color);
+      border-top: 0.125em solid var(--border-color);
     }
 
     blockquote,
@@ -113,8 +142,8 @@
       margin-right: 1em;
       margin-left: 1em;
       padding: 1em 0 1em 1em;
-      background: var(--field-background);
-      border-left: 0.25em solid var(--accent-color);
+      color: var(--field-background);
+      border-left: 0.25em solid var(--border-color);
 
       > :first-child {
         margin-top: 0;
@@ -133,7 +162,7 @@
       padding: 0.25em;
       font-size: inherit;
       font-family: "Courier New", Courier, monospace;
-      background: var(--field-background);
+      background-color: var(--field-background);
       border-radius: 0.25em;
     }
 
@@ -149,13 +178,13 @@
 
       thead {
         font-weight: bold;
-        background: var(--field-background);
+        background-color: var(--field-background);
       }
 
       td,
       th {
         padding: 0.5em;
-        border: 0.0625em solid var(--accent-color);
+        border: 0.0625em solid var(--border-color);
       }
     }
 
