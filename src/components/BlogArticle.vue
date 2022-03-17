9c7756d0562076380dfeb8fd4ba77a301fc2b5b5
Zachary <zsakowitz@zsnout.com>
2022-03-16 17:15:21 -0700

    Add `BlogArticle`

---
+++ src/components/BlogArticle.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+  import DocumentDisplay from "./DocumentDisplay.vue";
+  import Markdown from "./Markdown.vue";
+
+  defineProps<{ source: string }>();
+</script>
+
+<template>
+  <DocumentDisplay>
+    <Markdown :source="source" />
+  </DocumentDisplay>
+</template>
