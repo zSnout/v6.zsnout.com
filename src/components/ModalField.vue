9073576358fdb35d05c5506e1c459677fdd28cd0
Zachary <zsakowitz@zsnout.com>
2022-03-17 17:55:18 -0700

    Add ModalField

---
+++ src/components/ModalField.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+  defineProps<{ placeholder?: string; value?: string }>();
+</script>
+
+<template>
+  <input type="text" class="field" :placeholder="placeholder" :value="value" />
+</template>
+
+<style scoped lang="scss">
+  @use "@/assets/util.scss" as *;
+
+  .field {
+    display: inline-block;
+    box-sizing: border-box;
+    width: 100%;
+    padding: 0.5em 1em;
+    color: var(--text-color);
+    font-size: 1em;
+    background-color: var(--background);
+    border: none;
+    border-radius: 0.25em;
+
+    @include focus {
+      outline: 1px solid var(--border-color);
+    }
+  }
+</style>
