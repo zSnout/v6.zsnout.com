544c621163a9a8675b3b5128c4eb365bb2b9c5f3
Zachary <zsakowitz@zsnout.com>
2022-03-19 09:06:58 -0700

    Add `Title` component

---
+++ src/components/Title.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1 class="title">
+    <slot />
+  </h1>
+</template>
+
+<style scoped>
+  .title {
+    margin: 0 0 0.5em 0;
+    padding: 0 0.25em 0.25em;
+    color: var(--accent-color);
+    font-weight: 700;
+    font-family: "Montserrat", "Helvetica Neue", Helvetica, Arial, sans-serif;
+    border-bottom: 1px solid var(--accent-color);
+  }
+</style>
