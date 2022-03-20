9f49196e7ae6fcb422324671cd9002f74c2e1a6f
Zachary <zsakowitz@zsnout.com>
2022-03-20 10:50:25 -0700

    Add `Article` wrapper for DocumentDisplay

---
+++ src/components/Article.vue
@@ -0,0 +1,12 @@
+<script setup>
+  import DocumentDisplay from "./DocumentDisplay.vue";
+  import Prose from "./Prose.vue";
+</script>
+
+<template>
+  <DocumentDisplay>
+    <Prose>
+      <slot />
+    </Prose>
+  </DocumentDisplay>
+</template>
