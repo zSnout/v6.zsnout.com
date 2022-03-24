3fa7bf07dde1e46f16c78837564f377da7863595
Zachary <zsakowitz@zsnout.com>
2022-03-24 12:02:12 -0700

    Allow colors to be toggled

--- src/views/ColorRemoverView.vue
+++ src/views/ColorRemoverView.vue
@@ -1,24 +1,63 @@
 <script lang="ts" setup>
+  import CameraAdjuster from "@/components/CameraAdjuster.vue";
+  import NavLink from "@/components/NavLink.vue";
+  import { router } from "@/main";
   import { useRoute } from "vue-router";
-  import CameraAdjuster from "../components/CameraAdjuster.vue";
 
-  let colors = useRoute().params.colors;
+  let colors = "" + (useRoute().params.colors || "");
 
   let r = colors.includes("r");
   let g = colors.includes("g");
   let b = colors.includes("b");
+  let a = r && g && b;
+
+  function makeRoute() {
+    return `/color-remover/${r ? "r" : ""}${g ? "g" : ""}${b ? "b" : ""}`;
+  }
 
   function transform(data: ImageData) {
-    for (let i = 0; i < data.data.length; i += 4) {
-      if (r) data.data[i] = 0;
-      if (g) data.data[i + 1] = 0;
-      if (b) data.data[i + 2] = 0;
+    if (r) for (let i = 0; i < data.data.length; i += 4) data.data[i] = 0;
+    if (g) for (let i = 1; i < data.data.length; i += 4) data.data[i] = 0;
+    if (b) for (let i = 2; i < data.data.length; i += 4) data.data[i] = 0;
+
+    if (a) {
+      for (let i = 0; i < data.data.length; i += 4) {
+        let r = 360 * Math.random();
+
+        data.data[i] = r;
+        data.data[i + 1] = r;
+        data.data[i + 2] = r;
+      }
     }
 
     return data;
   }
+
+  function toggleRed() {
+    r = !r;
+    a = r && g && b;
+    router.push(makeRoute());
+  }
+
+  function toggleGreen() {
+    g = !g;
+    a = r && g && b;
+    router.push(makeRoute());
+  }
+
+  function toggleBlue() {
+    b = !b;
+    a = r && g && b;
+    router.push(makeRoute());
+  }
 </script>
 
 <template>
-  <CameraAdjuster :transform="transform" />
+  <CameraAdjuster :transform="transform">
+    <template #nav>
+      <NavLink @click="toggleRed">Toggle Red</NavLink>
+      <NavLink @click="toggleGreen">Toggle Green</NavLink>
+      <NavLink @click="toggleBlue">Toggle Blue</NavLink>
+    </template>
+  </CameraAdjuster>
 </template>
