5a7f5aec84ad038298ad2080c093ababadac6f4d
Zachary <zsakowitz@zsnout.com>
2022-03-24 17:01:13 -0700

    Add `ChessVersusStockfish`

---
+++ src/views/chess/ChessVersusStockfish.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+  import { analyze } from "@/assets/stockfish";
+  import ChessVersusAI from "@/components/ChessVersusAI.vue";
+  import type { ChessInstance } from "chess.js";
+
+  async function onMove(game: ChessInstance) {
+    let { bestMove } = await analyze(game);
+    return bestMove;
+  }
+</script>
+
+<template>
+  <ChessVersusAI :move="onMove" />
+</template>
