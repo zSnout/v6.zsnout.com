d54146190043d19abab2dfac386e857d8bd7692d
zsakowitz <zsakowitz@users.noreply.github.com>
2022-03-25 20:59:15 +0000

    deploy: 673bb6ad8ee0fad040e5d4cc944d18cf998d84b1

--- assets/ChessVersusStockfish.fb53ff8d.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./stockfish.2ec7d74b.js";import{_ as r}from"./ChessVersusAI.91942f27.js";import{d as n,o as s,c}from"./vendor.c311a53b.js";import"./index.d74c110e.js";import"./LegalMoveChessboard.555fd35a.js";const l=n({setup(m){async function t(o){let{bestMove:e}=await a(o);return e}return(o,e)=>(s(),c(r,{move:t}))}});export{l as default};
