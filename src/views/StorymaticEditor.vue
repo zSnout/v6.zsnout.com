a2c9cb22d0052e442029232e1e6fb8b74e69fa0c
Zachary <zsakowitz@zsnout.com>
2022-03-28 16:40:23 -0700

    Add `StorymaticEditor`

---
+++ src/views/StorymaticEditor.vue
@@ -0,0 +1,38 @@
+<script lang="ts" setup>
+  import DocumentDisplay from "@/components/DocumentDisplay.vue";
+  import StorymaticEditor from "@/components/StorymaticEditor.vue";
+  import TextConsole, { type Message } from "@/components/TextConsole.vue";
+  import { reactive } from "vue";
+
+  let messages: Message[] = reactive([]);
+</script>
+
+<template>
+  <DocumentDisplay explicit-height>
+    <div class="container">
+      <StorymaticEditor class="editor" />
+      <TextConsole class="console" :messages="messages" />
+    </div>
+  </DocumentDisplay>
+</template>
+
+<style scoped>
+  .container {
+    display: flex;
+    flex-direction: row;
+    gap: 1em;
+    height: 100%;
+  }
+
+  .editor {
+    flex: 1;
+    box-sizing: border-box;
+    width: 100%;
+    border-radius: 0.5em;
+  }
+
+  .console {
+    width: min(50%, 300px);
+    overflow: scroll;
+  }
+</style>
