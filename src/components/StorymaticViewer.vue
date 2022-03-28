2a711157c06e4441591170f8cc987504458e3558
Zachary <zsakowitz@zsnout.com>
2022-03-28 16:46:15 -0700

    Add seperate `StorymaticViewer` component

---
+++ src/components/StorymaticViewer.vue
@@ -0,0 +1,7 @@
+<script lang="ts" setup>
+  import TextConsole from "@/components/TextConsole.vue";
+</script>
+
+<template>
+  <TextConsole class="console" placeholder="Send messages to program..." />
+</template>
