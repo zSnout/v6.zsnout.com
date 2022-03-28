ed4e214fee8db12852d43f62aef83a1ce1c65b36
Zachary <zsakowitz@zsnout.com>
2022-03-28 08:03:45 -0700

    Add `StorymaticEditor`

---
+++ src/components/StorymaticEditor.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+  import ace from "ace-builds";
+  import aceLoc from "ace-builds/src-noconflict/ace.js?url";
+  import { computed } from "vue";
+  import { VAceEditor } from "vue3-ace-editor";
+
+  let props = defineProps<{ modelValue?: string }>();
+  let emit =
+    defineEmits<{ (event: "update:modelValue", value: string): void }>();
+
+  let model = computed<string>({
+    get() {
+      return props.modelValue || "";
+    },
+    set(value) {
+      emit("update:modelValue", value);
+    },
+  });
+
+  ace.config.set("basePath", new URL("./", `file:${aceLoc}`).pathname);
+</script>
+
+<template>
+  <VAceEditor class="editor" :print-margin="false" v-model:value="model" />
+</template>
