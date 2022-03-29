ae1163d33680be975a0de5d1409e246dcc4d9f77
zsakowitz <zsakowitz@users.noreply.github.com>
2022-03-29 00:22:53 +0000

    deploy: a06f1992dce90a9223ee5aaa69479a51f3be3545

--- assets/NotFoundView.73495642.js
+++
@@ -1,1 +0,0 @@
-import{D as o,N as t}from"./index.84f7671e.js";import{o as a,c as s,w as e,j as c,e as n,g as r}from"./vendor.c311a53b.js";const l=n("Home"),_=r("h1",null,"Well, you've found a broken link...",-1),m={setup(u){return(d,i)=>(a(),s(o,null,{nav:e(()=>[c(t,{to:"/"},{default:e(()=>[l]),_:1})]),default:e(()=>[_]),_:1}))}};export{m as default};
