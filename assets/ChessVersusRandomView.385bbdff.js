e2e17a1cb4ea9eca04e6aa2b8ec8365c61b7a4ed
zsakowitz <zsakowitz@users.noreply.github.com>
2022-04-04 15:09:08 +0000

    deploy: 228cefd7d5f93da06271e825e08bf72698e938b6

--- assets/ChessVersusRandomView.385bbdff.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./ChessVersusAI.8ab59c6f.js";import{d as n,o as m,c as a}from"./index.aa205489.js";import"./LegalMoveChessboard.7e397915.js";const i=n({setup(s){function t(e){let o=e.moves({verbose:!0});return o[Math.floor(Math.random()*o.length)]}return(e,o)=>(m(),a(r,{move:t}))}});export{i as default};
