c26042bf0aa2e5a3789cfd811196f2c2f90756ea
zsakowitz <zsakowitz@users.noreply.github.com>
2022-04-05 21:52:37 +0000

    deploy: 4fdb7196e680cd1f7d4724892a4c8f4f06fdca3a

--- assets/MultDivQuizView.d30ab7c0.js
+++
@@ -1,1 +0,0 @@
-import{Q as a}from"./Quiz.f23271f2.js";import{d as n,o as s,c}from"./index.e6e0fc39.js";const l=n({setup(f){function o(){let r=Math.floor(11*Math.random())+2,e=Math.floor(11*Math.random())+2,t=r*e;return Math.random()<.5?{prefix:`${r} \xD7 ${e} = `,answers:[t]}:{prefix:`${t} \xF7 ${r} = `,answers:[e]}}return(r,e)=>(s(),c(a,{generator:o}))}});export{l as default};
