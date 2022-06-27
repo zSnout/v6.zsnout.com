3cd0d01a57192a1f39fb14d954529cfd69b9e707
zsakowitz <zsakowitz@users.noreply.github.com>
2022-06-27 23:00:48 +0000

    deploy: eb7c3d739df681fd53d7dbe6144b9f0e835ec641

---
+++ assets/ChessVersusStockfish.8d3e5f66.js
@@ -0,0 +1,1 @@
+import{a}from"./stockfish.77ed8172.js";import{_ as s}from"./ChessVersusAI.e17aadf4.js";import{d as r,o as n,e as c}from"./index.82a198b5.js";import"./LegalMoveChessboard.6898641b.js";const u=r({__name:"ChessVersusStockfish",setup(m){async function t(e){let{bestMove:o}=await a(e);return o}return(e,o)=>(n(),c(s,{move:t}))}});export{u as default};
--- assets/SquareQuizView.894f98a0.js
+++
@@ -1,1 +0,0 @@
-import{Q as t}from"./Quiz.ae0b859a.js";import{d as o,o as n,e as s}from"./index.075875a4.js";const p=o({__name:"SquareQuizView",setup(u){function a(){let e=Math.floor(19*Math.random())+2,r=e**2;return Math.random()<.5?{prefix:`${e}\xB2 = `,answers:[r]}:{prefix:`\u221A${r} = `,answers:[e]}}return(e,r)=>(n(),s(t,{generator:a}))}});export{p as default};
